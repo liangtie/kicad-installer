e99e041cfc1ceae80ee8c5e564da6f9c826bceea
Ethan Chien <liangtie.qian@gmail.com>
2025-07-21 16:00:32 +0800

    feat(app): implement version check functionality
    - Add version check feature for the application
    - Implement get_latest_version function to fetch the latest version info
    - Use cpr and nlohmann/json libraries for network request and JSON parsing
    - Handle exceptions and return optional string with the latest version

--- app/utils/get_latest_version.cpp
+++ app/utils/get_latest_version.cpp
@@ -1,6 +1,27 @@
+#include <QMessageBox>
+#include <vector>
+
 #include "get_latest_version.h"
+
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+#include <qmessagebox.h>
 
 auto get_latest_version() -> std::optional<std::string>
 {
+  try {
+    cpr::Response r =
+        cpr::Get(cpr::Url {"https://kicad.eda.cn/kicad-versions.json"});
+    const auto versions =
+        nlohmann::json::parse(r.text).get<std::vector<std::string>>();
+    // The latest version is the last one in the list
+    if (!versions.empty()) {
+      return versions.back();
+    }
+  }
+
+  catch (...)
+  {
+  }
   return {};
 }
