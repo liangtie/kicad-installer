0e931eee55c294cf3092c3218ca75adaf6534c23
Ethan Chien <liangtie.qian@gmail.com>
2025-07-21 16:08:22 +0800

    feat(app): add download URL formatting for KiCad releases
    - Implement function to generate download URL from file name
    - Add logic to create base file name based on installation method
    - Update fmt_download_url function to use new formatting

--- app/utils/fmt_download_url.h
+++ app/utils/fmt_download_url.h
@@ -7,10 +7,22 @@
 
 #include "Installation_method.h"
 
+inline auto gen_url_from_file_name(std::string const& file_name) -> std::string
+{
+  return fmt::format("https://down.eda.cn/data/kicad-release/{}", file_name);
+}
+
 inline auto fmt_download_url(INSTALLATION_METHOD method,
                              std::string const& version) -> std::string
+
 {
-  return {};
+  auto base_file_name = fmt::format("kicad-huaqiu-{}-x86_64", version);
+
+  if (method == INSTALLER) {
+    base_file_name += ".exe";
+  }
+
+  return gen_url_from_file_name(base_file_name + ".zip");
 }
 
 #endif
